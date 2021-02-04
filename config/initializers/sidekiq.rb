80a0038650c142b2b9f2a19e8cf35561a6c6e04a
Armand Fardeau <fardeauarmand@gmail.com>
2021-02-04 10:51:10 +0100

    Fix offenses

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,15 +4,15 @@ require "sidekiq"
 
 Sidekiq.configure_server do |config|
   config.redis = {
-      url: ENV["REDIS_URL"],
-      namespace: "#{ENV["APP_NAME"]}-sidekiq"
+    url: ENV["REDIS_URL"],
+    namespace: "#{ENV["APP_NAME"]}-sidekiq"
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-      url: ENV["REDIS_URL"],
-      namespace: "#{ENV["APP_NAME"]}-sidekiq"
+    url: ENV["REDIS_URL"],
+    namespace: "#{ENV["APP_NAME"]}-sidekiq"
   }
 end
 
