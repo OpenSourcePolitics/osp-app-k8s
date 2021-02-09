0fcbb9b4c292d50fe64e737aaa05e10d2b467abe
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-02-09 12:39:02 +0100

    configure i18n locales in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,13 @@ require "decidim/dev"
 Decidim::Dev.dummy_app_path = File.expand_path(Rails.root.to_s)
 
 require "decidim/dev/test/base_spec_helper"
+
+RSpec.configure do |config|
+  config.before(:each) do
+    I18n.available_locales = %i(en ca es)
+    I18n.default_locale = :en
+    I18n.locale = :en
+    Decidim.available_locales = %i(en ca es)
+    Decidim.default_locale = :en
+  end
+end
