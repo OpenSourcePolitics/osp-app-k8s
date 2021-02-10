d896f4c32f3a8386ea24d560dbcbf72bf7d49815
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-02-10 10:24:33 +0100

    Cache assets in production (#34)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,13 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  if ENV["CACHE_ASSETS"].present?
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, s-maxage=31536000, max-age=15552000",
+      "Expires" => 1.year.from_now.to_formatted_s(:rfc822).to_s
+    }
+  end
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
