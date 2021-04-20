0b7638fdad45ef3f98a7953b0c8c4c8aab9e3dfa
Quentin Champ <26109239+Quentinchampenois@users.noreply.github.com>
2021-04-20 19:43:19 +0200

    Refactor ensure environment (#85)
    * refactor ensure environment
    
    * Fix lint issue
    
    Co-authored-by: Armand Fardeau <armandfardeau@users.noreply.github.com>
    Co-authored-by: Armand Fardeau <fardeauarmand@gmail.com>

--- config/initializers/01_ensure_environment.rb
+++ config/initializers/01_ensure_environment.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-if Rails.env.production?
+def required_env_vars
   %w(
     DATABASE_HOST
     DATABASE_NAME
     DATABASE_PASSWORD
     DATABASE_PORT
     DATABASE_USERNAME
-    MAPS_API_KEY
     REDIS_URL
     SCALEWAY_BUCKET_NAME
     SCALEWAY_ID
     SCALEWAY_TOKEN
     SECRET_KEY_BASE
-    SENTRY_DSN
     SMTP_ADDRESS
     SMTP_DOMAIN
     SMTP_PASSWORD
@@ -21,9 +19,23 @@ if Rails.env.production?
     RAILS_LOG_TO_STDOUT
     RAILS_SERVE_STATIC_FILES
     APP_NAME
-  ).each do |env_var|
+  )
+end
+
+def optional_env_vars
+  %w(SENTRY_DSN)
+end
+
+if Rails.env.production? && ENV["DISABLE_ENV_CHECKUP"].blank?
+  required_env_vars.each do |env_var|
     next unless !ENV.has_key?(env_var) || ENV[env_var].blank?
 
     raise "Missing environment variable: #{env_var}"
+  end
+
+  optional_env_vars.each do |env_var|
+    next unless !ENV.has_key?(env_var) || ENV[env_var].blank?
+
+    Rails.logger.warn "Missing environment variable: #{env_var}"
   end
 end
